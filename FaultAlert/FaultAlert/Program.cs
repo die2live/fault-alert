2b8006e3303ad5c53084287d7823eb14412adbcb
Alexandr Dermenji <alexandr.dermenji@amdaris.com>
2021-07-07 17:30:38 +0300

    initial console project

---
+++ FaultAlert/FaultAlert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaultAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
