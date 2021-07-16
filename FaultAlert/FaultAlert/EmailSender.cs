37dd245acef0dd2b4cd031006b7312c37809c2c7
Alexandr Dermenji <alexandr.dermenji@amdaris.com>
2021-07-16 16:42:06 +0300

    ISender

---
+++ FaultAlert/FaultAlert/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultAlert
+{
+    public class EmailSender : ISender
+    {
+        public bool Send(string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
